fbed424dc31a1544ffcebcec83cde59f56f5a3cf
Enoch <enoch@Enochs-Mac-Pro.local>
2017-06-28 19:00:34 -0500

    Updated build.gradle

--- app/src/main/java/com/example/enoch/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/example/enoch/flickrbrowser/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-
+import android.content.Context;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,4 +52,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
-
