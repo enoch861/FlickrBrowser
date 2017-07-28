62961935c0d9213796ffc7d22168a4d46b20b96b
Enoch <enoch861@hotmail.com>
2017-07-27 22:32:49 -0500

    Finished Material Design interface. NavigationBar color change is still wonky. Started working on creating search interface.

---
+++ app/src/main/java/com/example/enoch/flickrbrowser/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.example.enoch.flickrbrowser;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Enoch on 7/27/2017.
+ */
+
+public class BaseActivity extends ActionBarActivity {
+    private Toolbar mToolbar;
+    public static final String FLICKR_QUERY = "FLICKR QUERY";
+
+    protected Toolbar activateToolbar() {
+        if (mToolbar ==null){
+            mToolbar = (Toolbar) findViewById(R.id.app_bar);
+            if (mToolbar != null){
+                setSupportActionBar(mToolbar);
+            }
+        }
+        return mToolbar;
+    }
+
+    protected Toolbar activateToolbarWithHomeEnabled() {
+        activateToolbar();
+    if(mToolbar != null){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    return mToolbar;
+    }
+
+}
--- app/src/main/java/com/example/enoch/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/example/enoch/flickrbrowser/MainActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.content.Context;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private static final String LOG_TAG = "MainActivity";
     private List<Photo> mPhotosList = new ArrayList<Photo>();
@@ -22,13 +25,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activateToolbar();
+
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        ProcessPhotos processPhotos = new ProcessPhotos("android,nougat", true);
+        ProcessPhotos processPhotos = new ProcessPhotos("android, nougat", true);
         processPhotos.execute();
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public class ProcessPhotos extends GetFlickrJsonData {
         public ProcessPhotos(String searchCriteria, boolean matchAll) {
